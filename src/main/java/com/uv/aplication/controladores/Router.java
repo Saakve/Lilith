2902183ae1613dc54049a5923125e5ea7ea548c6
Kevin Adrian Avelino Sanchez <avesanke@gmail.com>
2023-06-12 21:01:08 -0600

    Se cambio la ruta del endpoint y ahora sirve estático

--- src/main/java/com/uv/aplication/controladores/Router.java
+++ src/main/java/com/uv/aplication/controladores/Router.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.uv.aplication.analizadores.IAnalizador;
 
-@CrossOrigin(origins = "https://salmon-beach-090d0c410.2.azurestaticapps.net")
+@CrossOrigin
 @RestController
 public class Router {
 
     @Autowired
     private IAnalizador analizador;
 
-    @GetMapping("/")
+    @GetMapping("/api/")
     public Respuesta getRespuesta(@RequestParam(value = "mensaje", defaultValue = "Hola") String entrada) {
         return new Respuesta(analizador.generarRespuesta(entrada));
     }
